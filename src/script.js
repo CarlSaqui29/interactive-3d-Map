15edd72e9ebee890f4aefee39aa69cb2583576c8
Carl Saqui <carlwilsonlaborsaqui@gmail.com>
2023-11-09 23:21:29 +0800

    fix see interior animation and controls

--- src/script.js
+++ src/script.js
@@ -312,8 +312,8 @@ function renderButtons_jasmine() {
         gsap.to(camera.position,{x: 0, y: 7, z: -18, duration: 2, ease: 'power3.inOut'})
         gsap.to(controls.target,{
             x: 0,
-            y: 0,
-            z: 100,
+            y: 10,
+            z: 10,
             duration: 2,
             ease: 'power3.inOut',
             onComplete() {
@@ -447,8 +447,8 @@ function renderButtons_linnea() {
         gsap.to(camera.position,{x: 0, y: 7, z: -18, duration: 2, ease: 'power3.inOut'})
         gsap.to(controls.target,{
             x: 0,
-            y: 0,
-            z: 100,
+            y: 10,
+            z: 10,
             duration: 2,
             ease: 'power3.inOut',
             onComplete() {
